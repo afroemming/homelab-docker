d35967cec0f96b4f2176706207088126168828d4
Amelia Froemming <7919407+afroemming@users.noreply.github.com>
2023-02-28 17:22:13 -0600

    remove scripts (managed by ansible now; see github/afroemming/homelab)

--- scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cd "${0%/*}"
-
-./init_logs.sh
-./init_secrets.sh
--- scripts/init_logs.sh
+++
@@ -1,9 +0,0 @@
-
-#! /bin/bash
-cd "${0%/*}"
-source ../.env
-
-mkdir $LOGDIR
-touch $LOGDIR/access.log  
-touch $LOGDIR/authelia.log  
-touch $LOGDIR/traefik.log
